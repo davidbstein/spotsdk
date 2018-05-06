08453db0e9dcd0cf159d8316d8c09a43c0715e0d
David Stein <davidbstein@gmail.com>
2018-05-06 22:22:35 +0000

    add a reference to browser-unpack pull request

--- scripts/unbundle.bash
+++ scripts/unbundle.bash
@@ -1,5 +1,9 @@
 #############################################
 # STUPID ECMA VERSION UPGRADER
+#
+# I've submitted a pull request to browser-unpack
+# that resolves this that can be checked at:
+# https://github.com/substack/browser-unpack/pull/19
 echo "TODO: automate this and remove the prompt"
 read -p "
 you must have gone to node_modules/browser-unpack/index.html and change ecmaVersion to 8. Continue (Y/n)? " -n 1 -r
@@ -22,10 +26,10 @@ for module_path in $SCRIPT_DIR/../.SpotifyModified.app/Contents/Resources/Apps/*
 do
   module=$(echo $module_path | tr "/" "\n" | tail -n1)
   echo_lightblue "unbundling $module"
-  #rm -rf $SCRIPT_DIR/../.unbundled/$module
-  #cp -r $SCRIPT_DIR/../.unzipped_raw/$module $SCRIPT_DIR/../.unbundled/$module
-  cd $SCRIPT_DIR/../.unzipped_raw/$module
-  $SCRIPT_DIR/../node_modules/browser-unpack/bin/cmd.js < bundle.js > unbundled.json
-  #npx browser-unpack < bundle.js > unbundled.json
+  rm -rf $SCRIPT_DIR/../.unbundled/$module
+  cp -r $SCRIPT_DIR/../.unzipped_raw/$module $SCRIPT_DIR/../.unbundled/$module
+  cd $SCRIPT_DIR/../.unbundled/$module
+  #$SCRIPT_DIR/../node_modules/browser-unpack/bin/cmd.js < bundle.js > unbundled.json
+  npx browser-unpack < bundle.js > unbundled.json
   cd -
 done;
