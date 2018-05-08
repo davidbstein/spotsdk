1a1d79556c6a523d4642dfb9093f2dcd243329d9
David Stein <davidbstein@gmail.com>
2018-05-08 10:38:17 +0000

    add module editor rough cut

---
+++ scripts/generate_editable_module.bash
@@ -0,0 +1,21 @@
+# Generates a "module" folder - which is the base for building an extension
+MODULE=$1
+TARGET="$(pwd)/$2"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $SCRIPT_DIR/color_functions.bash
+pushd .
+
+cp $SCRIPT_DIR/templates/module $TARGET/$MODULE
+cd $TARGET/$MODULE
+for subdir in $(find $SCRIPT_DIR/../.unbundled/$MODULE -type d | \
+                xargs python -c "import sys; print('\n'.join([a[len(sys.argv[1])+1:] for a in sys.argv[2:]]))")
+do
+  mkdir -p $subdir
+done
+
+mkdir src
+cp -r $SCRIPT_DIR/../.unzipped_raw/$MODULE .original
+rm .original/bundle.js
+cp -r $SCRIPT_DIR/../.sources/$MODULE/src .original/src
+
+popd
